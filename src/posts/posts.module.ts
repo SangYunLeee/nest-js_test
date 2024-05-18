63512f811fe7569227480119b9daa1a393e3ea91
sangyun lee <sororiri@gmail.com>
2024-05-19 00:04:17 +0900

    Fix: Import 에러 수정

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,12 +3,14 @@ import { PostsService } from './posts.service';
 import PostsController from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModel } from './entities/posts.entitiy';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   // eslint-disable-next-line prettier/prettier
   imports: [TypeOrmModule.forFeature([
     PostsModel
-  ])],
+  ]), AuthModule, UsersModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
