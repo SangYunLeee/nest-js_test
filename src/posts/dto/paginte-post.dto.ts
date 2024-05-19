68cfe16c68e51f334ded0bef9fe5628750e40e14
sangyun lee <sororiri@gmail.com>
2024-05-19 13:08:20 +0900

    Add: DTO 타입 자동형변환 추가

--- src/posts/dto/paginte-post.dto.ts
+++ src/posts/dto/paginte-post.dto.ts
@@ -1,8 +1,6 @@
-import { Type } from 'class-transformer';
 import { IsIn, IsNumber, IsOptional } from 'class-validator';
 
 export class PaginatePostDto {
-  @Type(() => Number)
   @IsNumber()
   @IsOptional()
   where__id__more_than: number = 0;
@@ -11,7 +9,6 @@ export class PaginatePostDto {
   @IsOptional()
   order__createdAt: 'ASC' | 'DESC' = 'ASC';
 
-  @Type(() => Number)
   @IsNumber()
   @IsOptional()
   take: number = 3;
