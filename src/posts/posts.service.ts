d9bdce1d870cfc975ad0a76f509e1397655bb4a2
sangyun lee <sororiri@gmail.com>
2024-05-20 04:08:53 +0900

    Fix: 미사용 코드 제거

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,11 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { FindOptionsWhere, LessThan, MoreThan, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { PostsModel } from './entities/posts.entitiy';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PaginatePostDto } from './dto/paginte-post.dto';
-import { ENV } from 'src/common/const/env.const';
 import { CommonService } from 'src/common/common.service';
 
 @Injectable()
@@ -73,82 +72,6 @@ export class PostsService {
 
   async paginatePosts(dto: PaginatePostDto) {
     return this.commonService.paginate(dto, this.postsRepository, {}, 'posts');
-  }
-
-  async pagePaginatePosts(dto: PaginatePostDto) {
-    const [posts, count] = await this.postsRepository.findAndCount({
-      order: {
-        createdAt: dto.order__createdAt,
-      },
-      take: dto.take,
-      skip: (dto.page - 1) * dto.take,
-    });
-
-    return { posts, total: count };
-  }
-
-  async cursorPaginatePosts(dto: PaginatePostDto) {
-    const where: FindOptionsWhere<PostsModel> = {};
-
-    if (dto.where__id__more_than || dto.where__id__less_than) {
-      where.id = dto.where__id__more_than
-        ? MoreThan(dto.where__id__more_than)
-        : LessThan(dto.where__id__less_than);
-    }
-
-    const posts = await this.postsRepository.find({
-      where,
-      order: {
-        createdAt: dto.order__createdAt,
-      },
-      take: dto.take,
-    });
-
-    const lastItem =
-      posts.length > 0 && posts.length === dto.take
-        ? posts[posts.length - 1]
-        : null;
-
-    const nextUrl = lastItem && new URL(`${ENV.BASE_URL}/posts`);
-
-    if (lastItem) {
-      for (const key of Object.keys(dto)) {
-        if (dto[key]) {
-          if (key !== 'where__id__more_than') {
-            nextUrl.searchParams.append(key, dto[key]);
-          }
-        }
-      }
-
-      let key: keyof PaginatePostDto = null;
-
-      if (dto.order__createdAt === 'ASC') {
-        key = 'where__id__more_than';
-      } else {
-        key = 'where__id__less_than';
-      }
-
-      nextUrl.searchParams.append(key, lastItem.id.toString());
-    }
-
-    /**
-     * Response
-     *
-     * data: Data[],
-     * cursor: {
-     *    after: 마지막 Data의 ID
-     * },
-     * count: 응답한 데이터의 갯수
-     * next: 다음 요청을 할때 사용할 URL
-     */
-    return {
-      data: posts,
-      cursor: {
-        after: lastItem?.id ?? null,
-      },
-      count: posts.length,
-      next: nextUrl?.toString() ?? null,
-    };
   }
 
   async generatePosts(userId: number) {
