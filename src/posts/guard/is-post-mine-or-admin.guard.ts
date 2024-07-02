e02ad93f310b2f13223f5d32ee89de8ef54271f8
sangyun lee <sororiri@gmail.com>
2024-07-02 15:25:20 +0900

    Fix: RBAC 상세설정 추가

--- src/posts/guard/is-post-mine-or-admin.guard.ts
+++ src/posts/guard/is-post-mine-or-admin.guard.ts
@@ -5,9 +5,10 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { PostsService } from '../posts.service';
+import { RolesEnum } from 'src/users/const/roles.const';
 
 @Injectable()
-export class IsPostMineOrAdmin implements CanActivate {
+export class IsPostMineOrAdminGuard implements CanActivate {
   constructor(private readonly postsService: PostsService) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
@@ -15,7 +16,15 @@ export class IsPostMineOrAdmin implements CanActivate {
     if (!user) {
       throw new UnauthorizedException('로그인이 필요합니다.');
     }
+    if (user.role === RolesEnum.ADMIN) {
+      return true;
+    }
     const { postId } = req.params;
+
+    const isOk = await this.postsService.isPostMine(Number(postId), user.id);
+    if (!isOk) {
+      throw new UnauthorizedException('권한이 없습니다.');
+    }
     return true;
   }
 }
